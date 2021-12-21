dc178000da3c1a54d9bf5994a4f3c1aa79d97ce0
Erin Borders <borders16@gmail.com>
2021-12-21 12:17:29 -0500

    trial text message set up and tested

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import os
+from dotenv import load_dotenv
 from twilio.rest import Client
+
+load_dotenv()
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='insert twilio trial number here',
+                     to='insert personal phone number here'
+                 )
+print(message.sid)
